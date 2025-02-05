1187238feab04b4eb111f50e231dea344920f945
m1587 <m05567271587@gmail.com>
2025-02-05 19:54:40 +0200

    change back ToDoDB

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1449319af1d14245587cc2657b1c5477ac67d22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9e4cb8c3931e4a12809f490f569a132f817f5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
